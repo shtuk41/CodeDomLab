782bd6aa21954d60fcb1e78548b497e9782f9942
Aleksander <shtuk41@hotmail.com>
2019-08-03 05:49:54 -0500

    Initial commit

---
+++ CodeDomLab/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.CodeDom;
+using System.Diagnostics;
+using System.CodeDom.Compiler;
+
+namespace CodeDomLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (System.CodeDom.Compiler.CompilerInfo ci in System.CodeDom.Compiler.CodeDomProvider.GetAllCompilerInfo())
+	        {
+		        foreach (string language in ci.GetLanguages())
+			        System.Console.Write("{0} ", language);
+			
+		        System.Console.WriteLine();
+
+	        }
+            Console.ReadKey();
+        }
+    }
+}
+
